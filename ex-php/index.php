6f9c5c095a2ce5dc007763b59287ef5f86a6081e
gabriele-annese <gannese06@gmail.com>
2021-12-21 14:46:00 +0100

    add es php

--- ex-php/index.php
+++ ex-php/index.php
@@ -7,22 +7,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>hello</h1>
-    <div class="container">
-        <div class="card">
-            <?php foreach ($database as $element) {?> 
-                <div class="img-ctn">
-                    <img src=" <?php echo $element['poster'] ?>" alt="">
-                </div>
-                <h3> 
-                    <?php echo $element['title'] ?>
-                </h3>
-
-            <?php } ?>
+    <header>
+        <div class="logo-ctn">
+            <img src="../img/logo.png" alt="">
         </div>
-    </div>
+    </header>
+    <main>
+        <section class="main-ctn">
+            <?php foreach ($database as $element) {?> 
+            <div class="card">
+                    <div class="img-ctn">
+                        <img src=" <?php echo $element['poster'] ?>" alt="">
+                    </div>
+                    <h3 class="title"> 
+                        <?php echo $element['title']; ?>
+                    </h3>
+                    <p class="author">
+                         <?php echo $element['author']; ?>
+                    </p>
+                    <p class="year">
+                        <?php echo $element['year']; ?>
+                    </p>
+                    <p class="genre">
+                        <?php echo $element['genre']; ?>
+                    </p>
+                </div>
+            <?php } ?>
+        </section>
+    </main>
 </body>
 </html>
